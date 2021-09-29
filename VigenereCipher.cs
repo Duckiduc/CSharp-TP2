02fe2a7afaa8c35e83c949bad7fa2757ba137c12
Duc-Thomas <duckiduc.dev@gmail.com>
2021-09-30 00:44:49 +0200

    Vigenere basic implementation (WIP)

--- VigenereCipher.cs
+++ VigenereCipher.cs
@@ -4,6 +4,40 @@ namespace VigenereCipher
 {  
     class VigenereCipher
 	{  
-		
+		private static bool CheckKey(string key, string text)
+        {
+            if (key.Length > text.Length)
+            {
+                return false;
+            } 
+            else
+            {
+                return true;
+            }
+        }
+
+        public static string Encrypt(string key, string text)
+        {
+            string EncryptedText = string.Empty;
+            for (int i = 0; i < text.Length; i++)
+            {
+                int letterIndex = (key[i] + text[i]) % 26;
+                letterIndex += 'A';
+                EncryptedText += (char)letterIndex;
+            }
+            return EncryptedText;
+        }
+
+        public static string Decrypt(string key, string text)
+        {
+            string DecryptedText = string.Empty;
+            for (int i = 0; i < key.Length && i < text.Length; i++)
+            {
+                int letterIndex = (text[i] - key[i] + 26) % 26;
+                letterIndex += 'A';
+                DecryptedText += (char)letterIndex;
+            }
+            return DecryptedText;
+        }
     }
 }
