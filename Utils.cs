9a518f5dfe52f5534931aa3f69421ad3a1cf822f
Faris Chtatou <faris.manage@gmail.com>
2021-10-01 17:16:39 +0200

    deleted repeated method

--- Utils.cs
+++ Utils.cs
@@ -2,16 +2,15 @@ using System;
   
 namespace CSharp_TP2
 {  
-    class Utils
+    public class Utils
 	{    
         // For all methods
-        public static bool isInputValid(string input) {
+        public static bool isValidString(string input) {
             for (int i = 0; i < input.Length; i++)
             {
                 if (char.IsDigit(input[i]))
                 {
-                    return false;
-               
+                    return false;  
                 }
             }
             return true;
@@ -23,25 +22,11 @@ namespace CSharp_TP2
             return int.TryParse(key, out number);
         }
 
-        // For Vigenere Cipher only
-        public static bool isKeyValidString(string key) {
-            for (int i = 0; i < key.Length; i++)
-            {
-                if (char.IsDigit(key[i]))
-                {
-                    return false;
-               
-                }
-            }
-            return true;
-        }
-
         // For all methods 
         public static bool isTypeValid(string st) {
             if((st == "Encrypt") ^ (st == "Decrypt")) {
                 return true;
-            } 
-            
+            }  
             return false;
         }
     } 
